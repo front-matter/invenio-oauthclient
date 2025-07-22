ce3a96ad27d641b9224f84378e7614e3245d824d
Martin Fenner <martin@front-matter.io>
2025-07-23 00:10:27 +0200

    Update pocketid.py

--- invenio_oauthclient/contrib/pocketid.py
+++ invenio_oauthclient/contrib/pocketid.py
@@ -209,9 +209,11 @@ def account_info(remote, resp):
         response = remote.get(user_info_url)
         user_info = get_dict_from_response(response)
         print("user_info:", user_info)
+        return user_info
     except Exception as e:
         print(f"Error fetching user info: {e}")
         user_info = {}
+        return user_info
 
     handlers = current_oauthclient.signup_handlers[remote.name]
     # `remote` param automatically injected via `make_handler` helper
